45e3c0305819c63df4907d720d72b1bea62071e0
Work <fraser.iain.lewis@gmail.com>
2025-12-14 11:18:55 +0800

    major updates - building strings from arguments to send to py

--- rtfp/R/neg_bin.R
+++ rtfp/R/neg_bin.R
@@ -71,7 +71,7 @@ cat("got=",py$a,"\n")
 #' @description A short description here. See the example below, currently this function fits the same negative binomial regression model as given in the rstanarm example. The data set is passed and the rest of the function is currently hard coded to this example, using one of the tensorflow probability samplers. The function is written in python and called via reticulate which also brings the MCMC sample data into R. The code is simple starting point for expansion. The same function could be written in Rstudio tfprobability library, but needs considerable care on the broadcasting.
 #' @param formula data.frame of the data to fit to the model - must be the dataset given in the example
 #' @param data description
-#' @param priors dddd
+#' @param custompriors dddd
 #' @returns a list of matrices of MCMC output, one member of the list for each estimated parameter, and each column in the matrix is the output of one chain. The number of columns in the matrix is the number of chains used. This is currently hardcoded. See function source.
 #' @examples
 #' \dontrun{
@@ -91,7 +91,7 @@ cat("got=",py$a,"\n")
 #' plot(phi_m[,3],type="l",col="skyblue",main="Trace plots")
 #'
 #' ## Density plots and compare with rstanarm
-#' par(mfrow=c(1,1))
+#' par(mfrow=c(1,2))
 #' plot(density(c(phi_m)),col="skyblue",lwd=2, main="rstanarm (orange) v TF (blue)",
 #' xlab="Reciprocal Dispersion") # all chains combined
 #'
@@ -109,26 +109,43 @@ cat("got=",py$a,"\n")
 #'  res_m<-as.matrix(stan_glm1)
 #'  lines(density(res_m[,"reciprocal_dispersion"]),col="orange",lwd=2)
 #'
+#'  phi_m<-samples[[3]] # the third parameter in the model, a matrix
+#'  plot(density(c(phi_m)),col="skyblue",lwd=2, main="rstanarm (orange) v TF (blue)",
+#'  xlab="treatment effect") # all chains combined
+#'  lines(density(res_m[,"treatment"]),col="orange",lwd=2)
 #'
 #' }
 
 #' @export
 glm_negbin<-function(formula=NULL,data=NULL,
-                      priors=NULL) {
+                      custompriors=NULL) {
+
+  # Extract the variables needed, including offset function, from the data
+  # and make it available to python. Order of cols is important
+  mf <- model.frame(formula=formula, data = data) # pass including offset
+  # e.g. y~a+b+offset(log(z)) and this gives df with all relevant variables
+  py$data<-r_to_py(mf) # this is needed as explicitly passes argument into py
+
+  #mypriorstring<-buildNBpriorstr(formula);
+  mystrs<-buildNBpriorstr(mf)
+  str1<-mystrs$str1
+  str2<-mystrs$str2
+  str3<-mystrs$str3
+  str4<-mystrs$str4
+  str5<-mystrs$str5
+  #str1<- "phi, beta_senior,beta_treatment, beta_roach1,alpha"
+  #str2<- "alpha,beta_roach1,beta_treatment,beta_senior,beta_expos"
+  #str3<- "alpha, beta_roach1,beta_treatment,beta_senior,phi"
 
   mypriorsstring<-r"(
   tfd.Normal(loc=0., scale=5., name="alpha"),
-  tfd.Normal(loc=0., scale=2.5, name="beta_roach"),
+  tfd.Normal(loc=0., scale=2.5, name="beta_roach1"),
   tfd.Normal(loc=0., scale=2.5, name="beta_treatment"),
   tfd.Normal(loc=0., scale=2.5, name="beta_senior"),
   tfd.Exponential(rate=1., name="phi"))"
 
-  mf <- model.frame(formula=formula, data = data) # pass including offset
-  # e.g. y~a+b+offset(log(z)) and this gives df with all relevant variables
-  py$data<-r_to_py(mf) # this is needed as explicitly passes argument into py
-  #print(py$data)
-  #return(mf)
 
+  # first part of py script - sets libraries and organizes the data passed
   stringpart1<-r"(
 
 import tensorflow as tf
@@ -142,71 +159,39 @@ import time
 rows, columns = data.shape
 
 y_data=tf.convert_to_tensor(data.iloc[:,0], dtype = tf.float32)
-#roach_data=tf.convert_to_tensor(data.iloc[:,1], dtype = tf.float32)
-#trt_data=tf.convert_to_tensor(data.iloc[:,2], dtype = tf.float32)
-#snr_data=tf.convert_to_tensor(data.iloc[:,3], dtype = tf.float32)
 
 X=tf.convert_to_tensor(data.iloc[:,1:],dtype=tf.float32)
-#print(X)
-#exit(1)
 X=tf.concat([tf.ones([rows,1],dtype=tf.float32), X], axis=1)
-
-#exposure_data=tf.math.log(tf.expand_dims(tf.convert_to_tensor(data.iloc[:,4],dtype=tf.float32),axis=-1))
-
-#X=tf.concat([X,exposure_data], axis=1)
 
 beta_expos=tf.convert_to_tensor(1.0,dtype=tf.float32) # dummy
 
 )"
 
-  stringpart2<-r"(
-def make_observed_dist(phi, beta_senior,beta_treatment, beta_roach,alpha):
+  stringpart2<-glue('
+def make_observed_dist({str1}):
     """Function to create the observed Normal distribution."""
-    #alpha_expanded = tf.expand_dims(alpha, -1)  # (3, 1)
-    #beta_roach_expanded = tf.expand_dims(beta_roach, -1)    # (3, 1)
-    #beta_treatment_expanded = tf.expand_dims(beta_treatment, -1)    # (3, 1)
-    #beta_senior_expanded = tf.expand_dims(beta_senior, -1)    # (3, 1)
-    #beta_expos_expanded = tf.expand_dims(1.0, -1)
-
-    #logmu = alpha_expanded + beta_roach_expanded * roach_data + beta_treatment_expanded * trt_data + beta_senior_expanded * snr_data +tf.math.log(exposure_data)
-
-    B=tf.stack([alpha,beta_roach,beta_treatment,beta_senior,beta_expos])
+    B=tf.stack([{str2}])
     #print(B._shape_as_list())
     logmu=tf.linalg.matvec(X,B)
 
     phi_expanded = tf.expand_dims(phi, -1)  # (3, 1)
     mu = tf.math.exp(logmu)
-    #r = tf$expand_dims(1.0, -1L)/ phi_expanded;  # total_count = 5
-    #probs <- r / (r + mu)
-
     prob = phi_expanded/(phi_expanded+mu)
-    #prob<-(phi_expanded)/(mu+phi_expanded)
+
     # Create distribution for observations
     return(tfd.Independent(
-        #tfd_normal(loc = mu, scale = sigma_expanded),
-        #mu = exp(mu)
-        #phi <- 0.2  # scale/overdispersion
-
-        #r = tf$expand_dims(1.0, -1L)/ sigma_expanded;  # total_count = 5
-        #probs <- r / (r + mu)
         tfd.NegativeBinomial(total_count = phi_expanded, probs = 1-prob),
         reinterpreted_batch_ndims = 1
     ))
 
-)"
+',.trim=FALSE)
 
   stringpart3<-r"(
 model = tfd.JointDistributionSequentialAutoBatched([
   )"
 
-  #tfd.Normal(loc=0., scale=5., name="alpha"),  # # Intercept (alpha)
-  #tfd.Normal(loc=0., scale=2.5, name="beta_roach"),  # # Slope (beta_roach1)
-  #tfd.Normal(loc=0., scale=2.5, name="beta_treatment"),  # # Intercept (alpha)
-  #tfd.Normal(loc=0., scale=2.5, name="beta_senior"),  # # Slope (beta_roach1)
-  #tfd.Exponential(rate=1., name="phi"),
-
   ## mypriorsstring in HERE
-
+  ## concat strings so far
   cumstring<-paste(stringpart1,stringpart2,stringpart3,mypriorsstring,",",sep="")
 
   stringpart4<-r"(
@@ -217,33 +202,32 @@ tf.random.set_seed(99999)
 
 )"
 
-  varstring<-r"(alpha, beta_roach,beta_treatment,beta_senior,phi)"
-
   stringpart5<-glue('
-def log_prob_fn({varstring}):
+def log_prob_fn({str3}):
   """Unnormalized target density as a function of states."""
   return model.log_prob((
-    {varstring}, y_data))
+    {str3}, y_data))
 
   ',.trim=FALSE)
 
-  varstring2<-r"(
-    alpha=pars[[0]]
-    beta_roach=pars[[1]]
-    beta_treatment=pars[[2]]
-    beta_senior=pars[[3]]
-    phi=pars[[4]])"
-
   stringpart6<-glue('
-def neg_log_prob_fn(pars):{varstring2}
-    """Unnormalized target density as a function of states."""
-    return -model.log_prob((
-      {varstring}, y_data))
+def neg_log_prob_fn(pars):
+{str4}
+   """Unnormalized target density as a function of states."""
+   return -model.log_prob((
+      {str3}, y_data))
 
 ',.trim=FALSE)
 
-  stringpart7<-r"(
-#print(log_prob_fn(0.1,0.2,0.3,0.5,0.1))
+mybijectorstr<-r"(
+    tfb.Identity(),
+    tfb.Identity(),
+    tfb.Identity(),
+    tfb.Identity(),
+    tfb.Exp()
+)"
+
+  stringpart7<-glue('
 start = tf.constant([0.1,0.2,0.3,0.5,0.1],dtype = tf.float32)
 #print(neg_log_prob_fn(start))
 
@@ -259,11 +243,8 @@ if(True):
 
 # bijector to map contrained parameters to real
 unconstraining_bijectors = [
-    tfb.Identity(),
-    tfb.Identity(),
-    tfb.Identity(),
-    tfb.Identity(),
-    tfb.Exp()
+{str5}
+
 ]
 
 num_results=20000
@@ -303,15 +284,18 @@ def do_sampling():
 
 
 t0 = time.time()
-#samples, kernel_results = do_sampling()
 samples = do_sampling()
 t1 = time.time()
-print("Inference ran in {:.2f}s.".format(t1-t0))
+',.trim=FALSE)
+
+stringpart8<-r"(
+
+print("InferencE ran in {:.2f}s.".format(t1-t0))
 
 samples = list(map(lambda x: tf.squeeze(x).numpy(), samples))
-
 )"
-bigstring<-paste(cumstring,stringpart4,stringpart5,stringpart6,stringpart7,sep="")
+
+bigstring<-paste(cumstring,stringpart4,stringpart5,stringpart6,stringpart7,stringpart8,sep="")
 #return(bigstring)
 py_run_string(bigstring)
 
---
+++ rtfp/R/utility_fns.R
@@ -0,0 +1,45 @@
+
+
+#' Extracts specified variables from a dataset based on a model formula.
+#' @param mf mode.frame
+#' @return A list of strings which be glued into python string
+buildNBpriorstr<-function(mf){
+  #return strings needed for negbin tfp model based on formula
+  varnames<-names(mf)[-1] #drop response
+  noms<-varnames[-grep("offset",varnames)] #drop offset
+  Xs<-paste("beta_",noms,sep="") # e.g. alpha, beta_roach,beta_treatment,beta_senior,phi
+
+  #print(Xs)
+  str1<-c("phi",Xs[length(Xs):1],"alpha")
+  str2<-c("alpha",Xs,"beta_expos")
+  str3<-str1[length(str1):1]
+  # phi, beta_senior,beta_treatment, beta_roach,alpha
+  # alpha,beta_roach,beta_treatment,beta_senior,beta_expos
+  # alpha, beta_roach,beta_treatment,beta_senior,phi
+
+  str4<-paste(paste(paste(paste("   ",str3,sep=""),"=pars[[",sep=""),0:(length(str3)-1),sep=""),"]]",sep="")
+
+  #alpha=pars[[0]]
+  #beta_roach1=pars[[1]]
+  #beta_treatment=pars[[2]]
+  #beta_senior=pars[[3]]
+  #phi=pars[[4]]
+
+  #tfb.Identity(),
+  #tfb.Identity(),
+  #tfb.Identity(),
+  #tfb.Identity(),
+  #tfb.Exp()
+
+  str5<-paste("   ",c(rep("tfb.Identity(),",length(str3)-1),"tfb.Exp()"),collapse="\n")
+
+  return(list(str1=paste(str1,collapse=","),
+              str2=paste(str2,collapse=","),
+              str3=paste(str3,collapse=","),
+              str4=paste(str4,collapse="\n"),
+              str5=str5))
+
+
+}
+
+
